0aca5676b94329a3689c82c34c995a534458ade9
omer1abay <abayomer1905@gmail.com>
2025-03-06 22:43:30 +0300

    Add project files.

---
+++ src/Domain/Entities/TodoItem.cs
@@ -0,0 +1,33 @@
+﻿namespace Todo_App.Domain.Entities;
+
+public class TodoItem : BaseAuditableEntity
+{
+    public int ListId { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Note { get; set; }
+
+    public PriorityLevel Priority { get; set; }
+
+    public DateTime? Reminder { get; set; }
+
+    public string? BackgroundColor { get; set; } = "#FFFFFF";
+
+    private bool _done;
+    public bool Done
+    {
+        get => _done;
+        set
+        {
+            if (value == true && _done == false)
+            {
+                AddDomainEvent(new TodoItemCompletedEvent(this));
+            }
+
+            _done = value;
+        }
+    }
+
+    public TodoList List { get; set; } = null!;
+}
---
+++ src/WebUI/ClientApp/aspnetcore-https.js
@@ -0,0 +1,34 @@
+// This script sets up HTTPS for the application using the ASP.NET Core HTTPS certificate
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const path = require('path');
+
+// ${process.env.APPDATA}/ASP.NET/https
+const baseFolder =
+  process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+    ? `C:/Users/Ömer Abay/AppData/Roaming/ASP.NET/https`
+    : `C:/Users/Ömer Abay/AppData/Roaming/.aspnet/https`;
+
+const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
+const certificateName = certificateArg ? certificateArg.groups.value : process.env.npm_package_name;
+
+if (!certificateName) {
+  console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
+  process.exit(-1);
+}
+
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+  spawn('dotnet', [
+    'dev-certs',
+    'https',
+    '--export-path',
+    certFilePath,
+    '--format',
+    'Pem',
+    '--no-password',
+  ], { stdio: 'inherit', })
+  .on('exit', (code) => process.exit(code));
+}
